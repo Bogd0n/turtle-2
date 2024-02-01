577e78f81a55e0d161a2b54bfbaecc0b4324239d
B0gdan <Bogdansmush@gmail.com>
2024-02-01 20:41:18 +0300

    квадрат и шестеренка

---
+++ kvadrat.py
@@ -0,0 +1,20 @@
+import turtle
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+
+
+a = 
+for i in range (15):
+    turtle.forward(a)
+    turtle.right(60)
+    a -= 20
+turtle.exitonclick()
+
+
+
